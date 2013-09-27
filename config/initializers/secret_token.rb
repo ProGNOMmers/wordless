780bc3410703aa5cccb7658830d15aac25b9de3e
Maurizio De Santis <desantis.maurizio@gmail.com>
2013-09-27 17:41:36 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wordless::Application.config.secret_key_base = '671ee74df6a624ae3ea89b34134bcd9dcea059762d21148c42601bf93249b825de338d6a79afda82c7cb913c6b7fe6286fffe790b90f09340a338802dec2b34f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wordless::Application.config.session_store :cookie_store, key: '_wordless_session'
